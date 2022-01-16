51ffacc542aeff02fd8c5a5c5b3d51d857f844c3
Imran Lala <imran.lala@discoverit.co.uk>
2022-01-16 18:58:24 +0000

    applied basic styling to success page

--- src/pages/FormTaskSuccess/FormTaskSuccess.js
+++ src/pages/FormTaskSuccess/FormTaskSuccess.js
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './FormTaskSuccess.module.scss';
 
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
 function FormTaskSuccess({ className, ...props }) {
     return (
         <div
             className={classNames(styles.formTaskSuccess, className)}
             {...props}>
-            <h1>Form Task Success</h1>
-            <p>You successfully submitted the form!</p>
+            <Header />
+            <div className={classNames(styles.content, className)} {...props}>
+                <h1>Form Task Success</h1>
+                <p>You successfully submitted the form!</p>
+            </div>
+            <Footer />
         </div>
     );
 }
