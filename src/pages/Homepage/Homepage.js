f24aa7624c6ef277fc583a4e3314b92c452f0804
Imran Lala <imran.lala@discoverit.co.uk>
2022-01-16 16:15:08 +0000

    added header and footer to home page and styled

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Homepage.module.scss';
 
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
 function Homepage({ className, ...props }) {
     return (
         <div className={classNames(styles.homepage, className)} {...props}>
-            <h1>Tech Test - React</h1>
+            <Header />
+            <div className={classNames(styles.content, className)} {...props}>
+                <h1>Tech Test - React</h1>
+            </div>
+            <Footer />
         </div>
     );
 }
